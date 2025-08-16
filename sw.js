41625d882265664efce031121d8df53b15e2775b
drmathememo <drmathememo@gmail.com>
2025-08-15 21:39:57 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,28 @@
-// Simple offline-first cache for Lentes 46
-const CACHE = 'lentes46-v1';
+const CACHE_NAME = "lentes46-cache-v2";
 const CORE = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png'
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.webmanifest"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(()=>self.skipWaiting()));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE))
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  const req = e.request;
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(req).then(res => res || fetch(req).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
-      return r;
-    }).catch(()=>caches.match('./index.html')))
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });

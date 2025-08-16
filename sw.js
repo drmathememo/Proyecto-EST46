d9843af405f31dbf9246380bd56cae3c8d7147e7
drmathememo <drmathememo@gmail.com>
2025-08-16 10:08:35 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,19 @@
-// Offline-first SW compatible con GitHub Pages subpaths
-const CACHE = 'lentes46-v3';
-const CORE_REL = [
-  'index.html',
-  'style.css',
-  'script.js',
-  'manifest.webmanifest',
-  'icons/icon-192.png',
-  'icons/icon-512.png',
-  'icons/apple-touch-icon.png'
-];
-const CORE = CORE_REL.map(p => new URL(p, self.location).toString());
-
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
+  e.waitUntil(
+    caches.open('proyecto-est46').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.webmanifest'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  const req = e.request;
   e.respondWith(
-    caches.match(req, {ignoreSearch:true}).then(res => res || fetch(req).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
-      return r;
-    }).catch(()=>caches.match(new URL('index.html', self.location).toString())))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });

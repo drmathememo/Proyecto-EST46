531ceb0c441726bc64e9e2d728e095a6376a034c
drmathememo <drmathememo@gmail.com>
2025-08-19 08:35:23 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,2 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('proyecto-est46').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/manifest.webmanifest'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+self.addEventListener('install', e=>{e.waitUntil(caches.open('est46').then(cache=>cache.addAll(['./','./index.html','./style.css','./app.js'])))});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)))});
